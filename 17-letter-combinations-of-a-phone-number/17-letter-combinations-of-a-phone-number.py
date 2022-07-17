1002d29ae78ac751dd50bf25c0032172abf65bda
Stefan Gogić <76776039+stefan-gg@users.noreply.github.com>
2022-07-17 02:49:51 +0200

    Time: 36 ms (82.63%), Space: 13.9 MB (30.72%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -3,8 +3,7 @@ class Solution:
         total_combinations = 1
         count = [0, 0, 0, 0]
         solution = []
-        cc = []    
-        
+        permutations = []    
         dict_ = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
 
         if digits == "":
@@ -18,28 +17,18 @@ class Solution:
 
         str_len = len(digits)
         count = count[0:str_len]
-        cc.append(count[:])
-        #while len(solution) != total_combinations:
+        permutations.append(count[:])
+
         for i in range(total_combinations):        
 
-
-            # combination = ""
-            # print(count)
-            # for number in range(str_len):
-
-            #     combination += dict_[digits[number]][count[number]]
-
-            # solution.append(combination)
             for c in range(len(count)-1, 0, -1):    
                 if count[c] == len(dict_[digits[c]])-1:
-
                     if c == 0:
                         if count[0] < len(dict_[digits[c]]):
 
                             count[0] += 1
                             for i in range(1, str_len+1):
                                 count[i] = 0
-
                     else:
                         count[c] = 0
                         count[c-1] += 1
@@ -51,17 +40,18 @@ class Solution:
                         if count[c] >= len(dict_[digits[c]]):
                             count[c-1] += 1
                             count[c] = 0
+
                 if not count[0] >= len(dict_[digits[0]]):
-                    cc.append(count[:])
+                    permutations.append(count[:])
 
                 if len(solution) == total_combinations:
                     break
 
-
-        for c in cc:
+        for permutation in permutations:
             combination = ""
-            for number in range(len(c)):
-                combination += dict_[digits[number]][c[number]]
+            
+            for number in range(len(permutation)):
+                combination += dict_[digits[number]][permutation[number]]
             solution.append(combination)
 
         return sorted(set(solution))
