e58b2bceb54b9e8f6d9e2375d907f21c0da7f3d7
Stefan Gogić <76776039+stefan-gg@users.noreply.github.com>
2022-07-18 21:55:27 +0200

    Time: 64 ms (14.93%), Space: 14 MB (24.32%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        brackets_dict = {"}":"{", "]":"[", ")":"("}
+
+        bracket_arr = []
+        
+        for bracket in s:
+            if bracket in brackets_dict.keys() and not bracket_arr:
+                return False
+        
+            elif not bracket in brackets_dict.keys():
+                bracket_arr.append(bracket)
+        
+            elif bracket_arr[-1] == brackets_dict[bracket]:
+                bracket_arr = bracket_arr[:-1]
+            
+            else:
+                return False
+        if not bracket_arr:
+            return True
+        return False
