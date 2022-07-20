57de815efe3dbfdbeb26affb469ce0c9e993e509
Stefan Gogić <76776039+stefan-gg@users.noreply.github.com>
2022-07-20 05:28:56 +0200

    Time: 156 ms (41.99%), Space: 15.8 MB (16.09%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # nums[:] ne pravi novi niz !!
         nums[:] = sorted(set(nums))
         return len(nums)
         
