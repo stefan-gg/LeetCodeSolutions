55e2a5aeeeffcad9526002ba47e73bc93857a2aa
Stefan Gogić <76776039+stefan-gg@users.noreply.github.com>
2022-07-20 13:08:37 +0200

    Time: 8393 ms (5.01%), Space: 13.9 MB (56.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,8 +1,10 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         
-        # for i in range(x // 2 + 1):
-        #     if i*i <= x and (i+1)*(i+1) > x:
-        #         return i
+        # int(sqrt(x)) je isto validno
         
-        return int(sqrt(x))
+        for i in range(x // 2 + 1):
+            if i*i <= x and (i+1)*(i+1) > x:
+                return i
+        
+        return x
