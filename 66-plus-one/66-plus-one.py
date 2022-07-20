b8c1543ed113f659afc080640a00914f34b7f450
Stefan Gogić <76776039+stefan-gg@users.noreply.github.com>
2022-07-20 12:28:04 +0200

    Time: 61 ms (22.81%), Space: 13.8 MB (96.58%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        index = -1
+        if digits.count(9) == len(digits):
+            
+            digits[0] = 1
+            digits[1:] = [0] * len(digits)
+            
+            return digits
+        
+        while digits[index] == 9:
+            digits[index] = 0
+            index -= 1
+        
+        digits[index] += 1
+        
+        return digits
